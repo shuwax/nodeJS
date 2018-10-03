627f661585327609c9b62efceb239c043649d915
Sebastian Cytawa <sebastian.cytawa@edulab.io>
2018-10-03 14:08:37 +0200

    Change response

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 //create route with method get,post,put,delete,patch (request,response) res.send(return value)
 app.get('/',(req,res) => {
-    res.send({hi:"there"});
+    res.send({tes:"test"});
 });
 const PORT = process.env.PORT || 5000;
 //with port to listen
