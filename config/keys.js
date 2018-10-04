f53655ebb4ed2fde0b651609864d08c4eb672827
Sebastian Cytawa <sebastian.cytawa@edulab.io>
2018-10-04 08:22:39 +0200

    Google Auth

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    googleClientID : '1000674252490-pia0d26nq5injsdqo4f8t3makh6ao1uj.apps.googleusercontent.com',
+    googleClientSecret: 'qeo7r9Az5dfhAQsKTEKV5AB2'
+};
--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 const app = express();
 
+//new istance of GoogleStrategy,para(conf)
+passport.use(
+    new GoogleStrategy(
+    {
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+    }, (accessToken,refreshToken,profile,done) => {
+        console.log('Access Token', accessToken);
+        console.log('Refresh Token',refreshToken);
+        console.log('Profile',profile);
+    })
+);
+app.get(
+    '/auth/google',
+    passport.authenticate('google',{
+        scope:['profile','email']
+    })
+);
+app.get('/auth/google/callback', passport.authenticate('google'));
 //create route with method get,post,put,delete,patch (request,response) res.send(return value)
-app.get('/',(req,res) => {
-    res.send({tes:"test"});
-});
+// app.get('/',(req,res) => {
+//     res.send({tes:"test"});
+// });
 const PORT = process.env.PORT || 5000;
 //with port to listen
 app.listen(PORT);
