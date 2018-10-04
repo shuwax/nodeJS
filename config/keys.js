86bdd48abfe3804cc3f79aec73541518c7d671ff
Sebastian Cytawa <sebastian.cytawa@edulab.io>
2018-10-04 10:44:11 +0200

    Add mongoose

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     googleClientID : '1000674252490-pia0d26nq5injsdqo4f8t3makh6ao1uj.apps.googleusercontent.com',
-    googleClientSecret: 'qeo7r9Az5dfhAQsKTEKV5AB2'
+    googleClientSecret: 'qeo7r9Az5dfhAQsKTEKV5AB2',
+    mongoURI : 'mongodb://shuwax:nokia123@ds223063.mlab.com:23063/edulab-dev'
 };
--- index.js
+++ index.js
@@ -1,33 +1,14 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
-const app = express();
+const mongoos = require('mongoose');
+const keys = require('./config/keys'); // import hidden key config
+//just load passport config
+require('./services/passport');
+require('./models/User');
 
-//new istance of GoogleStrategy,para(conf)
-passport.use(
-    new GoogleStrategy(
-    {
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-    }, (accessToken,refreshToken,profile,done) => {
-        console.log('Access Token', accessToken);
-        console.log('Refresh Token',refreshToken);
-        console.log('Profile',profile);
-    })
-);
-app.get(
-    '/auth/google',
-    passport.authenticate('google',{
-        scope:['profile','email']
-    })
-);
-app.get('/auth/google/callback', passport.authenticate('google'));
-//create route with method get,post,put,delete,patch (request,response) res.send(return value)
-// app.get('/',(req,res) => {
-//     res.send({tes:"test"});
-// });
+mongoos.connect(keys.mongoURI);
+
+const app = express();
+require('./routes/authRoutes')(app);
 const PORT = process.env.PORT || 5000;
 //with port to listen
 app.listen(PORT);
